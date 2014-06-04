0dbd53bc24d492ef930a2b0e0f0e0b617269b4ce
Daniel Smith <daniel.smith@bedrosians.com>
2014-06-04 00:28:07 -0700

    add demo

---
+++ demo/app.js
@@ -0,0 +1,20 @@
+var app = angular.module('app', ['ngResize']);
+
+app.directive('resizeTest', ['resize', function(resize){
+  return{
+    restrict:'A',
+    controller: function($scope){
+      resize.set();
+      $scope.$on('resize', function(data, $event){
+        $scope.size = $event;
+      });
+    },
+    template:'<li data-test-item ng-repeat="item in testitems">{{item.title}}</li>',
+    link:function($scope, $element, $attribute){
+      $scope.testitems = [];
+      for( var i = 0; i < 500; i++){
+        $scope.testitems.push({title:'test item ' + i, value:i});
+      }
+    }
+  };
+}]);
