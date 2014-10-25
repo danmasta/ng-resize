5cfeaf58b16a064e7a71aada182d96f3a9fcb94a
Daniel Smith <daniel@howlite.me>
2014-10-25 00:34:40 -0700

    update functionality, add provider, add api

--- ngresize.js
+++ ngresize.js
@@ -1,5 +1,5 @@
 /**
- * @license ngResize.js v1.0.0
+ * @license ngResize.js v1.1.0
  * (c) 2014 Daniel Smith http://www.danmasta.com
  * License: MIT
  */
@@ -11,66 +11,127 @@
  *
  * # ngResize
  *
- * The ngResize module provides an angular directive for handling resize events
- * expression is run when the window resizes.
- * The implementation is the same as default angular event handlers.
- * Event object is available as $event.
+ * ngResize module provides an angular directive, provider,
+ * and service for managing resize events in applications
+ *
+ * expressions are run when the window resizes and debounced
+ * event object is available as $event.
  */
 
 // define ngResize module
 var ngResize = angular.module('ngResize', []);
 
-// Factory for checking and binding resize event to window.
-//
-// Broadcasts 'resize' event from $rootScope which gets inherited
-// by all child scopes
-ngResize.factory('resize', ['$window', '$interval', '$rootScope', function($window, $interval, $rootScope) {
-  return {
-    checkBind: function() {
-      if (angular.isUndefined($rootScope.catchResize)) return false;
-      return $rootScope.catchResize;
-    },
-    setBind: function() {
-      var w = angular.element($window);
-      var resizeThrottle = 100;
-      var resized = false;
-      var timer = false;
-      w.on('resize', function(event) {
-        if (!resized) {
-          timer = $interval(function() {
-            if (resized) {
-              resized = false;
-              $interval.cancel(timer);
-              $rootScope.$broadcast('resize', {
-                width: $window.innerWidth,
-                height: $window.innerHeight
-              });
-            }
-          }, resizeThrottle);
-        }
-        resized = true;
+/*
+* ngResize Provider and Service
+*
+* provider allows to set throttle in config
+* and wether or not to bind on service initialization
+*
+* $broadcasts 'resize' event from $rootScope
+* which gets inherited by all child scopes
+*/
+ngResize.provider('resize', [function resizeProvider(){
+
+  // store throttle time
+  this.throttle = 100;
+
+  // by default bind window resize event when service
+  // is initialize/ injected for first time
+  this.initBind = 1;
+
+  // service object
+  this.$get = ['$rootScope', '$window', '$interval',  function($rootScope, $window, $interval){
+
+    var _this = this;
+
+    // api to set throttle amount
+    function setThrottle(throttle){
+      _this.throttle = throttle
+    };
+
+    // api to get current throttle amount
+    function getThrottle(){
+      return _this.throttle;
+    };
+
+    // trigger a resize event on provided $scope or $rootScope
+    function trigger($scope){
+      var $scope = $scope || $rootScope;
+      $rootScope.$broadcast('resize', {
+        width: $window.innerWidth,
+        height: $window.innerHeight
       });
-      w.triggerHandler('resize');
-      $rootScope.catchResize = true;
-    },
-    bind: function(){
-      if (!this.checkBind()) return this.setBind();
+    };
+
+    // bind to window resize event, will only ever be bound
+    // one time for entire app
+    var bound = 0;
+    var timer = 0;
+    var resized = 0;
+    function bind(){
+      if(!bound){
+        var w = angular.element($window);
+        w.on('resize', function(event) {
+          if (!resized) {
+            timer = $interval(function() {
+              if (resized) {
+                resized = 0;
+                $interval.cancel(timer);
+                trigger();
+              }
+            }, _this.throttle);
+          }
+          resized = 1;
+        });
+        bound = 1;
+        w.triggerHandler('resize');
+      }
+    };
+
+    // unbind window scroll event
+    function unbind(){
+      if(bound){
+        var w = angular.element($window);
+        w.off('resize');
+        bound = 0;
+      }
+    };
+
+    // by default bind resize event when service is created
+    if(_this.initBind){
+      bind();
     }
-  };
+
+    // return api
+    return{
+      getThrottle: getThrottle,
+      setThrottle: setThrottle,
+      trigger: trigger,
+      bind: bind,
+      unbind: unbind
+    };
+
+  }];
+
 }]);
 
-// ngResize directive, uses throttled resize event bound to window; will bind event only once.
-// Broadcasts 'resize' event from $rootScope
-//
-// Usage: ng-resize="expression()"
-// Event data is available as $event
-// Timeout is used to debounce any expressions to the end of the current digest
+/*
+* ngResize Directive
+*
+* uses a throttled resize event bound to $window;
+* will bind event only once for entire app
+* broadcasts 'resize' event from $rootScope
+*
+* usage: ng-resize="expression()"
+* event data is available as $event
+* $timeout is used to debounce any expressions
+* to the end of the current digest
+*/
 ngResize.directive('ngResize', ['$parse', '$timeout', 'resize', function($parse, $timeout, resize) {
   return {
     compile: function($element, attr) {
       var fn = $parse(attr['ngResize']);
       return function(scope, element, attr) {
-        resize.bind();
         scope.$on('resize', function(event, data) {
           $timeout(function() {
             scope.$apply(function() {
@@ -82,5 +143,6 @@ ngResize.directive('ngResize', ['$parse', '$timeout', 'resize', function($parse,
     }
   };
 }]);
+
 
 })(window, window.angular);
