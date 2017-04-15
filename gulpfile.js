3859ea396614fd9ced91b8d23030ab4c6ae8e152
Daniel Smith <dannmasta@gmail.com>
2017-04-15 15:21:04 -0700

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,131 +1,54 @@
-// dependencies
-var gulp = require('gulp');
 var path = require('path');
-var util = require('gulp-util');
- 
-// dev server
-var connect = require('connect');
-//var compression = require('compression');
-//var modRewrite = require('connect-modrewrite');
-var serveStatic = require('serve-static');
+var gulp = require('gulp');
+var express = require('express');
 var livereload = require('gulp-livereload');
- 
-// plugins
-//var bower = require('gulp-bower');
 var concat = require('gulp-concat');
 var deploy = require('gulp-gh-pages');
-//var less = require('gulp-less');
-//var sass = require('gulp-sass');
-//var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
-//var ngAnnotate = require('gulp-ng-annotate');
-//var templateCache = require('gulp-angular-templatecache');
-   
-  
- 
- 
- 
-/*
-* js
-*/
-var js = [
-  'index.js'
-];
+var annotate = require('gulp-ng-annotate');
 
 gulp.task('js', function(){
-	gulp.src(js)
-		.pipe(concat('ng-resize.js'))
-		.pipe(gulp.dest('dist'))
-		.pipe(uglify().on('error', util.log))
-		.pipe(concat('ng-resize.min.js'))
-		.pipe(gulp.dest('dist'));
+    return gulp.src('./index.js')
+    .pipe(annotate())
+    .pipe(concat('ng-resize.js'))
+    .pipe(gulp.dest('dist'));
 });
 
-
-var demo = [
-	'demo/**/*'
-];
-
-gulp.task('demo', function(){
-	gulp.src(demo)
-		.pipe(gulp.dest('build'));
-	gulp.src(['dist/**/*'])
-		.pipe(gulp.dest('build/dist'))
+gulp.task('js:min', function(){
+    return gulp.src('./index.js')
+    .pipe(annotate())
+    .pipe(uglify())
+    .pipe(concat('ng-resize.min.js'))
+    .pipe(gulp.dest('dist'));
 });
- 
- 
- 
- 
 
- 
- 
-/*
-* build
-*/
-gulp.task('build', ['js', 'demo']);
- 
- 
-/*
-* deploy
-*/
+gulp.task('build', ['js', 'js:min']);
+
 gulp.task('deploy', ['build'], function(){
-	return gulp.src('build/**/*.*').pipe(deploy({cacheDir:'pages'}));
+	return gulp.src(['dist/**/*', 'demo/**/*']).pipe(deploy({ cacheDir:'pages' }));
 });
- 
- 
-/*
-* server
-*/
-gulp.task('server', ['build'], function(next){
-	var server = connect();
-	//server.use(compression());
-	//server.use(modRewrite([
-	//	'!\\.html|\\.js|\\.css|\\.svg|\\.jp(e?)g|\\.png|\\.ico|\\.gif|\\.eot|\\.otf|\\.ttf|\\.woff|\\.mp4$ /index.html'
-	//]));
-	server.use(serveStatic('build'));
-	server.listen(3000, next);
+
+gulp.task('server', ['build'], function(done){
+
+    var app = express();
+
+    app.use(express.static('demo'));
+    app.use(express.static('dist'));
+    console.log('[development] dev server listening on: 8080');
+    app.listen(8080, done);
+
 });
-//gulp.task('server-rest', function(){
-//	var exec = require('child_process').exec;
-//	exec('node server\index.js', function(err, stdout, stderr){
-//		if(err){
-//			
-//		} else {
-//			console.log(stdout);
-//			console.log(stderr);
-//		}
-//	});
-//});
- 
- 
-/*
-* watch
-*/
+
 gulp.task('watch', ['server'], function(){
-	livereload.listen();
 
-	//gulp.watch(partials, ['partials']);
-	//gulp.watch(files, ['files']);
-	//gulp.watch(img, ['img']);
-	//gulp.watch('app/less/**/*.scss', ['css']);
-	gulp.watch(js, ['js']);
-	gulp.watch(demo, ['demo']);
-	//gulp.watch(fonts, ['fonts']);
-	//gulp.watch(bower_css, ['bower_css']);
-	//gulp.watch(bower_js, ['bower_js']);
-	//gulp.watch(bower_fonts, ['bower_fonts']);
+    livereload.listen();
 
-	gulp.watch(['build/**/*'], function(file){
-		livereload.changed(file.path);
-	});
-	
-//	gulp.watch('server/**/*', ['server-rest'], function(file){
-//		console.log('rest server file changed', file.path);
-//	});
+    gulp.watch('./index.js', ['js', 'js:min']);
+
+    gulp.watch(['demo/**/*', 'dist/**/*'], function(file){
+        livereload.changed(file.path);
+    });
+
 });
- 
- 
-/*
-* default
-*/
+
 gulp.task('default', ['watch']);
