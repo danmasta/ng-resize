e151478e130f9a283c9ebca776322e097df8f440
Daniel Smith <dannmasta@gmail.com>
2018-05-12 12:44:00 -0700

    fix gulpfile spacing

--- gulpfile.js
+++ gulpfile.js
@@ -9,23 +9,23 @@ var annotate = require('gulp-ng-annotate');
 
 gulp.task('js', function(){
     return gulp.src('./index.js')
-    .pipe(annotate())
-    .pipe(concat('ng-resize.js'))
-    .pipe(gulp.dest('dist'));
+        .pipe(annotate())
+        .pipe(concat('ng-resize.js'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js:min', function(){
     return gulp.src('./index.js')
-    .pipe(annotate())
-    .pipe(uglify())
-    .pipe(concat('ng-resize.min.js'))
-    .pipe(gulp.dest('dist'));
+        .pipe(annotate())
+        .pipe(uglify())
+        .pipe(concat('ng-resize.min.js'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', ['js', 'js:min']);
 
 gulp.task('deploy', ['build'], function(){
-	return gulp.src(['dist/**/*', 'demo/**/*']).pipe(deploy({ cacheDir:'pages' }));
+    return gulp.src(['dist/**/*', 'demo/**/*']).pipe(deploy({ cacheDir:'pages' }));
 });
 
 gulp.task('server', ['build'], function(done){
