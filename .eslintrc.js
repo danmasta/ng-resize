f4f88bcbd7802f6fdf642dccebcef1dc8561ad4e
Daniel Smith <dannmasta@gmail.com>
2018-05-12 12:42:45 -0700

    add editorconfig, eslint, update gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "warn"
+        ],
+        "no-undef": [
+            "warn"
+        ],
+        "no-unused-vars": [
+            "warn"
+        ]
+    }
+};
